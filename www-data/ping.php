0fc849dd60dcd02fcb3e200feeae5dd63f9cdfc9
4k1 <23312005+4k1@users.noreply.github.com>
2018-09-17 10:39:46 +0900

    Added default content

---
+++ www-data/ping.php
@@ -0,0 +1,6 @@
+<?php
+
+    header( "HTTP/1.1 200 OK" );
+    print ( "<html><head>");
+    print ( '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">');
+    print ( "</head><h2><i style='color: #1E90FF' class='far fa-thumbs-up'></i>&nbsp;Your browser has successfully reached to this API server!</h2></html>" );
